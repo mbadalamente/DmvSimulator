7a867fe31aa2e79c33445c2991a1525c09e969bf
mbadalamente <70178700+mbadalamente@users.noreply.github.com>
2021-03-01 18:57:25 -0500

    set up the DMV simulator

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,4 +1,14 @@
 
 public class DmvSimulator {
+	public static void main(String[] args) {
+		System.out.println("Welcome to the DMV!");
+		int personNumber = (int) (Math.random() * 100);
+		System.out.println("Please wait for your number to be called. Your number is " + personNumber + ".");
+		for (int i = personNumber + 1; i != personNumber; i = (i % 100) + 1) {
+			System.out.println("Now helping: " + i);
+		}
+		System.out.println("Now helping: " + personNumber);
+		System.out.println("Sorry, you do not have what we need to help you at this time.");
 
+	}
 }
