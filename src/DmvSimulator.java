645966f8911d6992626eecded085d54fde302b2d
mbadalamente <70178700+mbadalamente@users.noreply.github.com>
2021-03-02 13:35:06 -0500

    increased the wait time from 100 to 200

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -2,9 +2,9 @@
 public class DmvSimulator {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the DMV!");
-		int personNumber = (int) (Math.random() * 100);
+		int personNumber = (int) (Math.random() * 200);
 		System.out.println("Please wait for your number to be called. Your number is " + personNumber + ".");
-		for (int i = personNumber + 1; i != personNumber; i = (i % 100) + 1) {
+		for (int i = personNumber + 1; i != personNumber; i = (i % 200) + 1) {
 			System.out.println("Now helping: " + i);
 		}
 		System.out.println("Now helping: " + personNumber);
